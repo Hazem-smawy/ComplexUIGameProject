98032608378d463a2c17be79d4f4b3e83d6ef4a0
hazem-smawy <hazemsmawy@gmail.com>
2022-10-11 15:09:03 +0300

    Initial Commit

---
+++ ComplexUIGameProject/ComplexUIGameProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexUIGameProjectApp.swift
+//  ComplexUIGameProject
+//
+//  Created by hazem smawy on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ComplexUIGameProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
